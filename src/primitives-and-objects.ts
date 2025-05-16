866828ecafead64054a7e3a304955979d3bb689d
Oleksii Kryvtsov <nightmarelie@gmail.com>
2025-05-15 20:56:38 -0600

    feat: add nested schema

--- src/primitives-and-objects.ts
+++ src/primitives-and-objects.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import {z} from 'zod';
 
 const numberOfGuests = z.number().min(1).max(3);
 const contactEmail = z.string().email();
@@ -7,11 +7,19 @@ console.log(numberOfGuests.parse(3));
 console.log(contactEmail.parse('test@gmail.com'));
 
 
+
+const guestDetailsSchema = z.object({
+    name: z.string(),
+    age: z.number().min(18).max(100),
+    email: z.string().email(),
+});
+
 const roomBookingSchema = z.object({
     roomType: z.string(),
     dueDate: z.date(),
     numberOfGuests: z.number(),
     price: z.number().positive(),
+    guestDetails: z.array(guestDetailsSchema),
 });
 
 const bookingData = {
@@ -19,6 +27,13 @@ const bookingData = {
     dueDate: new Date('2023-10-12'),
     numberOfGuests: 2,
     price: 100,
+    guestDetails: [
+        {
+            name: 'John Doe',
+            age: 25,
+            email: 'john@gmail.com',
+        }
+    ]
 };
 
 const parsedData = roomBookingSchema.parse(bookingData);
