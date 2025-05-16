d60025e900ac6e9c41e9bceaa794e15ff409ce97
Oleksii Kryvtsov <nightmarelie@gmail.com>
2025-05-15 20:51:30 -0600

    feat: add string schema

--- src/primitives-and-objects.ts
+++ src/primitives-and-objects.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
 const numberOfGuests = z.number().min(1).max(3);
+const contactEmail = z.string().email();
 
 console.log(numberOfGuests.parse(3));
+console.log(contactEmail.parse('test@gmail.com'));
