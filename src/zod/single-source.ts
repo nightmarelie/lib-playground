50bc6f07076df07814e326ae5eb30d318daa2f41
Oleksii Kryvtsov <nightmarelie@gmail.com>
2025-05-15 21:12:33 -0600

    refactor: add single type

---
+++ src/zod/single-source.ts
@@ -0,0 +1,10 @@
+import {z} from 'zod';
+
+const guestDetailsSchema = z.object({
+    name: z.string(),
+    age: z.number().min(18).max(100),
+    email: z.string().email(),
+});
+
+type GuestDetails = z.infer<typeof guestDetailsSchema>;
+
