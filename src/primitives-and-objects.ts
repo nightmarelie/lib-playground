d6d766dcc783f490e1e5393ce624c39a566415c1
Oleksii Kryvtsov <nightmarelie@gmail.com>
2025-05-15 20:53:36 -0600

    feat: add object schema

--- src/primitives-and-objects.ts
+++ src/primitives-and-objects.ts
@@ -5,3 +5,22 @@ const contactEmail = z.string().email();
 
 console.log(numberOfGuests.parse(3));
 console.log(contactEmail.parse('test@gmail.com'));
+
+
+const roomBookingSchema = z.object({
+    roomType: z.string(),
+    dueDate: z.date(),
+    numberOfGuests: z.number(),
+    price: z.number().positive(),
+});
+
+const bookingData = {
+    roomType: 'single',
+    dueDate: new Date('2023-10-12'),
+    numberOfGuests: 2,
+    price: 100,
+};
+
+const parsedData = roomBookingSchema.parse(bookingData);
+
+console.log(parsedData);
