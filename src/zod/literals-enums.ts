3f0a99452ca2b19f84c1d7d6acaeef7190fbbf47
Oleksii Kryvtsov <nightmarelie@gmail.com>
2025-06-02 21:41:32 -0600

    feat(zod): add literal type

--- src/zod/literals-enums.ts
+++ src/zod/literals-enums.ts
@@ -1,17 +1,28 @@
 import {z} from "zod";
 
-const bookingSchema = z.object({
-    userId: z.number(),
-    id: z.number(),
-    title: z.string(),
-    body: z.string(),
-});
-
 // define a literal for a specific booking ID
 
 const standardRoomLiteral = z.literal('standard-room');
 
 type T1 = z.infer<typeof standardRoomLiteral>;
+
+const bookingSchema = z.object({
+    roomType: standardRoomLiteral,
+    id: z.number(),
+    title: z.string(),
+    body: z.string(),
+});
+
+const bookingData = {
+    roomType: 'standard-room',
+    id: 1,
+    title: 'Booking Title',
+    body: 'Booking Body',
+}
+
+const parsedBooking = bookingSchema.parse(bookingData);
+
+console.log(parsedBooking);
 
 
 
