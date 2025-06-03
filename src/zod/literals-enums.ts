a14b44bd5eb9c011d5907c62a390958c1793cd75
Oleksii Kryvtsov <nightmarelie@gmail.com>
2025-06-02 21:42:33 -0600

    feat(zod): add enum type

--- src/zod/literals-enums.ts
+++ src/zod/literals-enums.ts
@@ -6,15 +6,21 @@ const standardRoomLiteral = z.literal('standard-room');
 
 type T1 = z.infer<typeof standardRoomLiteral>;
 
+const RoomTypeEnum = z.enum([
+    'standard-room',
+    'deluxe-room',
+    'suite-room',
+])
+
 const bookingSchema = z.object({
-    roomType: standardRoomLiteral,
+    roomType: RoomTypeEnum,
     id: z.number(),
     title: z.string(),
     body: z.string(),
 });
 
 const bookingData = {
-    roomType: 'standard-room',
+    roomType: 'deluxe-room',
     id: 1,
     title: 'Booking Title',
     body: 'Booking Body',
